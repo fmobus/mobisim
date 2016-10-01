8fe2d593e6b9c1b9ef50416aa5456cffdc7e08e3
Felipe Mobus <fmobus@thoughtworks.com>
2016-10-01 01:01:06 -0300

    enforcing heading end-cap for traces; enforcing heading as starter part

--- src/lib/tracer.js
+++ src/lib/tracer.js
@@ -1,5 +1,21 @@
 // import { zipObject } from 'lodash';
 
+export class Tracer {
+  constructor(trace) {
+    if (trace[0] != "H") { throw "First instruction part must be a heading"; }
+    this.parts = trace.split(" ").map(Part.build);
+    this.parts.push(new HeadingPart(this.changeOfHeading() + this.parts[0].heading));
+  }
+  toString() {
+    return this.parts.map(x => x.toString()).join(" ");
+  }
+  changeOfHeading() {
+    let sum = (a,b) => a + b;
+    return this.parts.map(x => x.changeOfHeading()).reduce(sum, 0)
+  }
+
+}
+
 export class Part {
   static build(instruction) {
     let cmd = instruction[0]
@@ -15,15 +31,21 @@ export class Part {
   constructor(cmd) {
     this.cmd = cmd;
   }
-  alike(other) {
+  mergeable(other) {
     return other.cmd == this.cmd
+  }
+  changeOfHeading() {
+    return 0;
   }
 }
 
 class HeadingPart extends Part {
   constructor(heading) {
     super('H');
-    this.heading = heading;
+    this.heading = Math.round(heading);
+  }
+  toString() {
+    return "H" + this.heading
   }
 }
 
@@ -35,6 +57,9 @@ class StraightPart extends Part {
   merge(other) {
     return new StraightPart(this.dist + other.dist);
   }
+  toString() {
+    return "F" + this.dist;
+  }
 }
 
 class CurvePart extends Part {
@@ -43,20 +68,32 @@ class CurvePart extends Part {
     this.radius = radius;
     this.dist   = dist
   }
-  alike(other) {
-    return super.alike(other) && other.radius == this.radius;
+  mergeable(other) {
+    return super.mergeable(other) && other.radius == this.radius;
   }
   merge(other) {
     return new CurvePart(this.cmd, this.radius, this.dist + other.dist);
   }
+  toString() {
+    return `${this.cmd}${this.radius},${this.dist}`
+  }
+  changeOfHeading() {
+    let sign = (this.cmd == 'L')? -1 : 1;
+    return to_deg(this.dist / this.radius) * sign;
+  }
 }
 
-export const Tracer = (trace) => {
-  let parts = trace.split(" ").map(Part);
+function to_deg(angle) {
+  return angle * 180 / Math.PI;
+}
+function to_rad(angle) {
+  return angle * Math.PI / 180;
+}
+function cos(angle) {
+  return Math.cos(to_rad(angle));
+}
+function sin(angle) {
+  return Math.sin(to_rad(angle));
+}
 
-  return {
-    append: (other) => {
 
-    }
-  }
-};
--- test/tracer.js
+++ test/tracer.js
@@ -6,7 +6,18 @@ describe("Tracer operations", () => {
   describe("Part parser", () => {
     let straight = Part.build("F30");
     let curveRight = Part.build("R30,100");
-    let curveLeft  = Part.build("L60,200");
+    let curveLeft  = Part.build("L60,90");
+    let heading    = Part.build("H45");
+
+    describe("heading", () => {
+      it("has toString", () => {
+        expect(heading.toString()).to.be("H45");
+      });
+      it("rounds to nearest integer", () => {
+        let fractional = Part.build("H57.3");
+        expect(fractional.toString()).to.be("H57");
+      });
+    });
 
     describe("straight", () => {
       it("has F as command", () => {
@@ -21,13 +32,16 @@ describe("Tracer operations", () => {
         expect(result.cmd).to.be('F');
         expect(result.dist).to.be(430);
       });
-      it("is alike any other straight", () => {
+      it("is mergeable any other straight", () => {
         let other = Part.build("F400");
-        expect(straight.alike(other)).to.be.ok();
+        expect(straight.mergeable(other)).to.be.ok();
       });
       it("is unlike any other command", () => {
         let other = Part.build("R400,100");
-        expect(straight.alike(other)).to.not.be.ok();
+        expect(straight.mergeable(other)).to.not.be.ok();
+      });
+      it("has toString", () => {
+        expect(straight.toString()).to.be("F30");
       });
     });
 
@@ -42,16 +56,16 @@ describe("Tracer operations", () => {
         expect(curveRight.radius).to.be(30)
         expect(curveLeft.radius).to.be(60)
       });
-      it("only alike if same radii and direction", () => {
+      it("only mergeable if same radii and direction", () => {
         let other = Part.build("R30,200");
-        expect(curveRight.alike(other)).to.be.ok();
+        expect(curveRight.mergeable(other)).to.be.ok();
       });
       it("unlike if different radii same direction", () => {
         let other = Part.build("R500,200");
-        expect(curveRight.alike(other)).to.not.be.ok();
+        expect(curveRight.mergeable(other)).to.not.be.ok();
       });
       it("unlike if same radii different direction", () => {
-        expect(curveRight.alike(curveLeft)).to.not.be.ok();
+        expect(curveRight.mergeable(curveLeft)).to.not.be.ok();
       });
 
       it("merges two curves of same radii and direction", () => {
@@ -60,14 +74,46 @@ describe("Tracer operations", () => {
         expect(result.radius).to.be(30);
         expect(result.dist).to.be(300);
       });
+      it("has toString", () => {
+        expect(curveRight.toString()).to.be("R30,100");
+        expect(curveLeft.toString()).to.be("L60,90");
+      });
+
+      it("change of heading", () => {
+        expect(curveRight.changeOfHeading()).to.be.eql(190.9859317102744);
+        expect(curveLeft.changeOfHeading()).to.be(-85.94366926962348);
+      });
     });
+
   });
 
-  // let base = Tracer("H0 F100 H0");
-  // describe("append", () => {
-  //   it("same operations are merged", () => {
-  //     let result = base.append("F30");
-  //     expect(result.toString(), "H0 F130 H0");
-  //   });
-  // });
+  describe("Construction", () => {
+    let straight = new Tracer("H0 F100");
+    let curve    = new Tracer("H0 R100,100");
+    let mixed    = new Tracer("H100 L60,90 R30,100 F100");
+
+    it("trace must start with heading", () => {
+      let fn = function() { return new Tracer("F200"); };
+      expect(fn).to.throwException();
+    });
+    it("always has an end-cap heading", () => {
+      expect(straight.parts.length).to.be(3);
+    });
+    it("end-cap heading has the final direction", () => {
+      expect(straight.toString()).to.be("H0 F100 H0");
+      expect(curve.toString()).to.be("H0 R100,100 H57");
+      expect(mixed.toString()).to.be("H100 L60,90 R30,100 F100 H205");
+    });
+    it("change of heading sums up all changes of heading from a trace's parts", () => {
+      expect(straight.changeOfHeading()).to.be(0);
+      expect(curve.changeOfHeading()).to.be(57.29577951308232);
+      expect(mixed.changeOfHeading()).to.be(105.04226244065092);
+    });
+  });
+  describe("Appending", () => {
+    xit("same operations are merged", () => {
+      let result = base.append("F30");
+      expect(result.toString(), "H0 F130 H0");
+    });
+  });
 });
